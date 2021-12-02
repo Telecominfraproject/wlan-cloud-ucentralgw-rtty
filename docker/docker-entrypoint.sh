3622c6f2eab82d04516e993b493f31b57ba40fa4
Max Brenner <xamrennerb@gmail.com>
2021-12-02 14:28:30 +0100

    improve stability of Docker entrypoint
    Signed-off-by: Max Brenner <xamrennerb@gmail.com>

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,8 +5,10 @@ if [ -e /rttys/rttys.conf ]; then
     # randomize rttys http username and password
     USERNAME=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 20)
     PASSWORD=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 20)
-    sed "s/http-username:.*/http-username: ${USERNAME}/g" /rttys/rttys.conf > tmp && cat tmp > /rttys/rttys.conf
-    sed "s/http-password:.*/http-password: ${PASSWORD}/g" /rttys/rttys.conf > tmp && cat tmp > /rttys/rttys.conf
+    temp_file=$(mktemp)
+    # ignore errors as filesystem may be mounted as read only
+    sed "s/http-username:.*/http-username: ${USERNAME}/g" /rttys/rttys.conf > $temp_file && cat $temp_file > /rttys/rttys.conf || true
+    sed "s/http-password:.*/http-password: ${PASSWORD}/g" /rttys/rttys.conf > $temp_file && cat $temp_file > /rttys/rttys.conf || true
 fi
 
 exec "$@"
